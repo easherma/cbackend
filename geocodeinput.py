57643cd2f6411cdce1f59da9488e435a74f05309
Eric Sherman <easherma@users.noreply.github.com>
2016-08-02 17:20:51 -0500

    Update geocodeinput.py

--- geocodeinput.py
+++ geocodeinput.py
@@ -9,20 +9,23 @@ with open('./out6_file3_address_3_clean.csv', 'rb') as f:
     reader = csvkit.reader(f)
     your_list = list(reader)
     print your_list[0][0]
+    
+# input address
+
+print "Enter address to geocode: "
+address = raw_input()
 
 #geocode
-results = []
-for i, val in enumerate(your_list):
-    address = [val][0][0]
-    params= {'text':address}
+def geocodeInput(address):
+    results = []
     url = 'http://localhost:3100/v1/search?'
     r = requests.get(url + 'text=' + address)
-    rjson = r.json()['features'][0]
-    rjson['properties']['query'] = address
-    results.append(rjson)
+    print r.json()
+    #rjson = r.json()['features'][0]
+    #rjson['properties']['query'] = address
+    #results.append(rjson)
 
-with open('./out6_file3_address_3_clean.json', 'wb') as fd:
-    fd.write(json.dumps(results))
-~
-#from postal.parser import parse_address
-#parse_address('The Book Club 100-106 Leonard St Shoreditch London EC2A 4RH, United Kingdom')
+def writeOutput(filename):
+    with open('./' + filename+ '.json', 'wb') as fd:
+        fd.write(json.dumps(results))
+
